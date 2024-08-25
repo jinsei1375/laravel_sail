fa87f31fad5752b6175d9fc30327ce08900f2be8
Shinsei <murakami.shinsei.zn@gmail.com>
2024-08-26 08:20:51 +0900

    Carbon::parse()の引数nullだとnow()

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $settingDate = Carbon::parse($setting->date);
+    $carbon = Carbon::parse('2021-01-01 00:00:00');
+    $nullCarbon = Carbon::parse(null);
+    return view('welcome')
+        ->with([
+            'carbon' => $carbon,
+            'nullCarbon' => $nullCarbon,
+        ]);
 });
